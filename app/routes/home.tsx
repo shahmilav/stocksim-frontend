f051853b23571b3af2306be1f365bd78652a3fbc
milav <shahmilav@gmail.cm>
2024-12-10 16:25:58 -0500

    Move tradehistory to new file, clean up user interface.

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,39 +1,37 @@
 import {
-  Table,
   Title,
-  Text,
+  Space,
+  Card,
   AppShell,
   Group,
-  Burger,
-  Modal,
   Button,
   Drawer,
 } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
-import PortfolioTable from "app/components/PortfolioTable";
-import AccountData from "app/components/AccountData";
-import Trade from "app/components/Trade";
-import { Suspense, useState } from "react";
 import { Await, useLoaderData } from "@remix-run/react";
-import { redirect, data} from "@remix-run/node";
+import Navbar from "app/components/Navbar";
+import PortfolioTable from "app/components/PortfolioTable";
+import TradeHistory from "app/components/TradeHistory";
+import AccountData from "app/components/AccountData";
+import TradeContainer from "app/components/Trade";
+import { Suspense, useState } from "react";
+import { defer, redirect } from "@remix-run/node";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
- 
   const response = await fetch("http://localhost:3000/user", {
     headers: request.headers,
     credentials: "include",
   });
 
-
   let user = null;
   if (!response.ok) {
     return redirect("/");
-    console.log(response)
+    console.log(response);
   } else {
     user = await response.json();
   }
 
- const portfolio = fetch("http://localhost:3000/portfolio", {
+  const portfolio = fetch("http://localhost:3000/portfolio", {
     credentials: "include",
     headers: request.headers,
   }).then((res) => res.json());
@@ -48,76 +46,13 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
     headers: request.headers,
   }).then((res) => res.json());
 
-  return data({ portfolio, account, transactions, user });
+  return defer({ portfolio, account, transactions, user });
 };
-
-interface TransactionHistoryProps {
-  data: {
-    id: string;
-    timestamp: string;
-    stock_symbol: string;
-    quantity: number;
-    transaction_type: string;
-    price: number;
-  }[];
-}
-function TransactionHistory({ data }: TransactionHistoryProps) {
-  const formatDate = (utcString) => {
-    const utcDate = new Date(utcString + "Z");
-
-    const options = {
-      month: "2-digit",
-      day: "2-digit",
-      year: "numeric",
-      hour: "numeric",
-      minute: "2-digit",
-      hour12: true,
-    };
-
-    return utcDate.toLocaleString("en-US", options);
-  };
-
-  const rows = data.map((element) => (
-    <Table.Tr key={element.id}>
-      <Table.Td>{formatDate(element.timestamp)}</Table.Td>
-      <Table.Td>{element.stock_symbol}</Table.Td>
-      <Table.Td>{element.transaction_type}</Table.Td>
-      <Table.Td>{element.quantity}</Table.Td>
-      <Table.Td>
-        {new Intl.NumberFormat("en-US", {
-          style: "currency",
-          currency: "USD",
-        }).format(element.price / 100)}
-      </Table.Td>
-      <Table.Td>
-        {new Intl.NumberFormat("en-US", {
-          style: "currency",
-          currency: "USD",
-        }).format((element.quantity * element.price) / 100)}
-      </Table.Td>
-    </Table.Tr>
-  ));
-  return (
-    <Table withTableBorder>
-      <Table.Thead>
-        <Table.Tr>
-          <Table.Th>Timestamp</Table.Th>
-          <Table.Th>Symbol</Table.Th>
-          <Table.Th>Trade Type</Table.Th>
-          <Table.Th>Quantity</Table.Th>
-          <Table.Th>Purchase Price</Table.Th>
-          <Table.Th>Cash Value</Table.Th>
-        </Table.Tr>
-      </Table.Thead>
-      <Table.Tbody>{rows}</Table.Tbody>
-    </Table>
-  );
-}
-
 export default function Home() {
-  const [openedBurger, { toggle }] = useDisclosure();
+  // Open and close the trade dialog.
   const [opened, { open, close }] = useDisclosure(false);
 
+  // Open and close the trade history drawer.
   const [drawerOpened, setDrawerOpened] = useState(false);
 
   const { account, portfolio, transactions, user } =
@@ -132,28 +67,12 @@ export default function Home() {
       navbar={{
         width: 250,
         breakpoint: "sm",
-        collapsed: { mobile: !opened },
+        collapsed: { mobile: true },
       }}
       padding="md"
     >
       <AppShell.Header>
-        <Burger
-          opened={openedBurger}
-          onClick={toggle}
-          hiddenFrom="sm"
-          size="sm"
-        />
-
-        <Group m={10}>
-          <Button radius="md" variant="default" onClick={open}>
-            Trade
-          </Button>
-          <Text order={1} fw="400">
-            Welcome, {user.name}
-          </Text>
-
-          <a href="http://localhost:3000/logout">Logout</a>
-        </Group>
+        <Navbar user={user} open={open} />
       </AppShell.Header>
 
       <AppShell.Navbar p="md">
@@ -165,6 +84,14 @@ export default function Home() {
       </AppShell.Navbar>
 
       <AppShell.Main>
+        <Card withBorder radius="md" shadow="md" hiddenFrom="sm">
+          <Suspense fallback={<div>Loading...</div>}>
+            <Await resolve={account}>
+              {(account) => <AccountData data={account} />}
+            </Await>
+          </Suspense>
+        </Card>
+        <Space hiddenFrom="sm" h="lg" />
         <Suspense fallback={<div>Loading...</div>}>
           <Await resolve={portfolio}>
             {(portfolio) => (
@@ -178,46 +105,45 @@ export default function Home() {
           </Await>
         </Suspense>
 
-        <Modal
+        <TradeContainer
           opened={opened}
-          radius="md"
-          onClose={close}
-          withCloseButton={false}
-        >
-          <Trade
-            close={close}
-            symbol={symbol}
-            setSymbol={setSymbol}
-            action={action}
-            setAction={setAction}
-          />
-        </Modal>
+          close={close}
+          symbol={symbol}
+          setSymbol={setSymbol}
+          action={action}
+          setAction={setAction}
+        />
 
-        <Suspense fallback={<div>Loading...</div>}>
-          <Button
-            onClick={() => setDrawerOpened(true)}
-            variant="default"
-            mt={20}
-            radius="md"
-          >
-            Trade History
-          </Button>
-          <Drawer
-            position="bottom"
-            opened={drawerOpened}
-            offset={8}
-            radius="md"
-            withCloseButton={false}
-            onClose={() => setDrawerOpened(false)}
-          >
-            <Title mb={15} order={2} fw="400">
+        <Group justify="flex-end">
+          <Suspense fallback={<div>Loading...</div>}>
+            <Button
+              onClick={() => setDrawerOpened(true)}
+              variant="filled"
+              color="#3861f6"
+              mt={20}
+              size="compact-md"
+              style={{ fontWeight: 500 }}
+              radius="lg"
+            >
               Trade History
-            </Title>
-            <Await resolve={transactions}>
-              {(transactions) => <TransactionHistory data={transactions} />}
-            </Await>
-          </Drawer>
-        </Suspense>
+            </Button>
+            <Drawer
+              position="bottom"
+              opened={drawerOpened}
+              offset={8}
+              radius="md"
+              withCloseButton={false}
+              onClose={() => setDrawerOpened(false)}
+            >
+              <Title mb={15} order={2} fw="400">
+                Trade History
+              </Title>
+              <Await resolve={transactions}>
+                {(transactions) => <TradeHistory data={transactions} />}
+              </Await>
+            </Drawer>
+          </Suspense>
+        </Group>
       </AppShell.Main>
     </AppShell>
   );
